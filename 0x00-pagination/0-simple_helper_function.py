f0cd65b75b678d5c8a2af42c877650781b06d6af
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-31 02:53:13 -0400

    editing first task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,5 +5,5 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     firstIdx = (page-1) * page_size
-    secondIdx = page * page_size
+    secondIdx = ((page-1) * page_size) + page_size
     return (firstIdx, secondIdx)
