7e23545b9f98af609e87f4ceca7755def780979a
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-31 02:50:40 -0400

    inital

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Pagination function returns indexes"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    firstIdx = (page-1) * page_size
+    secondIdx = page * page_size
+    return (firstIdx, secondIdx)
