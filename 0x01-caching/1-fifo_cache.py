d4c93c80ecc6c8bf029f4b46385fb4da91a0b28f
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-03 08:54:31 -0400

    fifo cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Basic Dictionary Cache"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Basic cache implementation"""
+    def put(self, key, item):
+        """put method for the cache system"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            pass
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_added = list(self.cache_data.keys())[0]
+            del self.cache_data[last_added]
+            print(f"DISCARD: {last_added}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get method for the cache system"""
+        if key is None:
+            return None
+        res = self.cache_data.get(key)
+        if res is None:
+            return None
+        return res
