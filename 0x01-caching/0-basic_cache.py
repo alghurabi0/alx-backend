b10daa2d1101b6e8ed6436692efb331b151011f5
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-03 08:33:57 -0400

    fjkldsj

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,12 +4,15 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """Basic cache implementation"""
     def put(self, key, item):
+        """put method for the cache system"""
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """get method for the cache system"""
         if key is None:
             return None
         res = self.cache_data.get(key)
