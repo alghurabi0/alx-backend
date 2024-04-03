e756007670ee75e953115f535e76ec2186cba0f4
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-03 09:15:57 -0400

    mru cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Basic Dictionary Cache"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Basic cache implementation"""
+    def __init__(self):
+        """Init of an instance"""
+        super().__init__()
+        self.cache_data = OrderedDict(self.cache_data)
+
+    def put(self, key, item):
+        """put method for the cache system"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_added = self.cache_data.popitem(last=True)
+            print(f"DISCARD: {last_added[0]}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get method for the cache system"""
+        if key is None:
+            return None
+        res = self.cache_data.get(key)
+        if res is None:
+            return None
+        self.cache_data.move_to_end(key)
+        return res
