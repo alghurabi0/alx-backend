cdba7e0941531df201f2e0cf95f804f465d6fd0d
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-09 11:40:26 -0400

    localeselector

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Flask application"""
+import babel
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    '''Config class'''
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+@babel.localeselector
+def get_locale() -> str:
+    """determine bet lang match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home() -> str:
+    """home page route handler"""
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    """init of script"""
+    app.run(debug=True)
