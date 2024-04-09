32554e04c20a81284feec563e115e6eade66e333
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-09 11:14:12 -0400

    first task

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,11 +5,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home() -> str:
     """home page route handler"""
     return render_template("0-index.html")
 
+
 if __name__ == "__main__":
     """init of script"""
-    app.run()
+    app.run(debug=True)
