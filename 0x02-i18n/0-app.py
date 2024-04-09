d42780c45e24a56cbb9b38e69bd7743af7969e5e
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-09 11:10:25 -0400

    type

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def home() -> str:
     """home page route handler"""
     return render_template("0-index.html")
 
