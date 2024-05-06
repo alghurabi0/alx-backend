31404863231122aaf92dc6c23b4d9e718d554323
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-05-06 11:48:53 -0400

    async

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+import { promisify } from 'util'
+
+const client = redis.createClient();
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+})
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const whatever = promisify(client.get).bind(client);
+  const value = await whatever(schoolName);
+  console.log(value);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
